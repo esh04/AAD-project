0afd2b0b7909883f0060d4520b2fb62008c41a15
Esh <eshika.k@research.iiit.ac.in>
2021-11-25 20:13:14 +0530

    sock game complete

--- PyScripts/divide_and_conquer.py
+++ PyScripts/divide_and_conquer.py
@@ -9,10 +9,10 @@ def rand_num(i,j):
 def guess_socks(correct, guess, wrong_answers, max):
     if wrong_answers < max:
         if guess == correct:
-            return "You are correct! It was {}".format(correct), wrong_answers
+            return "You are correct! It was sock number {}".format(correct), wrong_answers
         else:
             if wrong_answers == max:
-                return  "Game over. The correct answer was {}. Press Restart to play again".format(correct), wrong_answers
+                return  "Game over. The smelly sock was sock {}. Press Restart to play again".format(correct), wrong_answers
 
             if guess>correct:
                 return "Your guess was too high. You have {} more tries".format(math.ceil(max) - wrong_answers), wrong_answers + 1
--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def divide_and_conquer():
     session["total_socks"] = rand_num(10,50)
     session["max_tries"] = math.log(session["total_socks"],2)
     session["num"] = rand_num(1,10)
-    session["text"] = "Guess a number between 1 and {}".format(session["total_socks"])
+    session["text"] = "Guess among the socks numbered  1 to {}.".format(session["total_socks"])
     session["wrong_answers"] = 0
     print("get")
     return render_template("divide-and-conquer.html")
@@ -30,7 +30,7 @@ def divide_and_conquer():
       session["guess"] = int(request.form.get("sock"))
       session["text"], session["wrong_answers"] = guess_socks(session["num"], session["guess"], session["wrong_answers"],session["max_tries"])
     else:
-          session["text"] = "Please choose an option."
+          session["text"] = "Please choose a sock."
     return render_template("divide-and-conquer.html")
       
 
